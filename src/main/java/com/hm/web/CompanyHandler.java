d45d23fced2c43613b9fc78066ec865f605b11fb
wuweihuang <605182955@qq.com>
2019-09-23 09:28:40 +0800

    吴伟煌0920 11:17 公司，管理员登录界面

--- src/main/java/com/hm/web/CompanyHandler.java
+++ src/main/java/com/hm/web/CompanyHandler.java
@@ -1,8 +1,6 @@
 package com.hm.web;
 
 
-import com.hm.entity.Staff;
-import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
